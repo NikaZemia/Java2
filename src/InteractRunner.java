7e3628ae4777e00e8f714851cead176520e35861
NikaZemia <NikaZemia@gmail.com>
2018-05-02 12:22:10 +0300

    commit

--- src/InteractRunner.java
+++ src/InteractRunner.java
@@ -1,40 +1,24 @@
 import java.util.Scanner;
 
 public class InteractRunner {
+
+        static Calculator calc;
+        static String first, second, act, exit;
+
         public static void main(String[] args) {
                 Scanner reader = new Scanner(System.in);
                 try{
-                        Calculator calc = new Calculator();
-                        String exit = "n";
+                        calc = new Calculator();
+                        exit = "n";
                         while(!exit.equals("y")){
                                 System.out.println("Enter first arg: ");
-                                String first = reader.next();
+                                first = reader.next();
                                 System.out.println("Enter second arg: ");
-                                String second = reader.next();
+                                second = reader.next();
                                 System.out.println("Select action: sumary(+:1), minus(-:2), multiplay(*:3), divide(/:4)");
-                                String act = reader.next();
-                                switch (act){
-                                        case "+":
-                                        case "1":
-                                                calc.add(Integer.valueOf(first), Integer.valueOf(second));
-                                                break;
-                                        case "-":
-                                        case "2":
-                                                calc.minus(Integer.valueOf(first), Integer.valueOf(second));
-                                                break;
-                                        case "*":
-                                        case "3":
-                                                calc.multiplay(Integer.valueOf(first), Integer.valueOf(second));
-                                                break;
-                                        case "/":
-                                        case "4":
-                                                calc.divide(Integer.valueOf(first), Integer.valueOf(second));
-                                                break;
-                                                default:
-                                                        System.out.println("Do not action selected");
-                                }
-
-                                System.out.println("result: " + calc.getResult());
+                                act = reader.next();
+                                Boolean action = VuborAction(act, first, second);
+                                ShowResults(action);
                                 calc.cleanResults();
                                 System.out.println("Exit: yes(y)/no(n)");
                                 exit = reader.next();
@@ -42,6 +26,42 @@ public class InteractRunner {
                 }
                 finally{
                         reader.close();
+                }
+        }
+
+        public static boolean VuborAction(final String action, final String first, final String second){
+                boolean tof = false;
+                switch (action){
+                        case "+":
+                        case "1":
+                                calc.add(Integer.valueOf(first), Integer.valueOf(second));
+                                tof = true;
+                                break;
+                        case "-":
+                        case "2":
+                                calc.minus(Integer.valueOf(first), Integer.valueOf(second));
+                                tof = true;
+                                break;
+                        case "*":
+                        case "3":
+                                calc.multiplay(Integer.valueOf(first), Integer.valueOf(second));
+                                tof = true;
+                                break;
+                        case "/":
+                        case "4":
+                                calc.divide(Integer.valueOf(first), Integer.valueOf(second));
+                                tof = true;
+                                break;
+                        default:
+                                System.out.println("Do not action selected");
+                                tof = false;
+                }
+                return tof;
+        }
+
+        public static void ShowResults(final boolean tof){
+                if(tof){
+                        System.out.println("result: " + calc.getResult());
                 }
         }
 }
